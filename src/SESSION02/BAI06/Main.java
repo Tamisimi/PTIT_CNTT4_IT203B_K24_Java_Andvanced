8b192c6fa809855a5dba1d0b088f17212af301b6
Tamisimi <alexmd0107@gmail.com>
2026-03-09 22:57:54 +0700

    Create Main.java

---
+++ src/SESSION02/BAI06/Main.java
@@ -0,0 +1,15 @@
+package session02.hw06;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        UserProcessor processor = UserUtils::convertToUpperCase;
+        User user = new User("linhngoc");
+
+        String result = processor.process(user);
+        System.out.println(result);
+
+    }
+
+}
