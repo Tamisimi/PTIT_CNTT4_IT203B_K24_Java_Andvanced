4d71f09b95fe9d05895da4b1655957938cd1fe9d
Tamisimi <alexmd0107@gmail.com>
2026-03-10 23:17:32 +0700

    Update BAI02.java

--- src/SESSION03/BAI02/BAI02.java
+++ src/SESSION03/BAI02/BAI02.java
@@ -12,9 +12,9 @@ public class BAI02 {
 
     public static void main(String[] args) {
         User[] users = {
-            new User("alice",   "alice@gmail.com",   Status.ACTIVE),
-            new User("bob",     "bob@yahoo.com",     Status.INACTIVE),
-            new User("charlie", "charlie@gmail.com", Status.ACTIVE)
+            new User("loc","loc@gmail.com",Status.ACTIVE),
+            new User("minh","minh@yahoo.com",Status.INACTIVE),
+            new User("thang", "thang@gmail.com",Status.ACTIVE)
         };
 
         Arrays.stream(users)
