095b224d1dc31bdda96a86e5e627a48abff5f8c3
Tamisimi <alexmd0107@gmail.com>
2026-03-10 23:35:51 +0700

    Update and rename BAI02.java to Main.java

---
+++ src/SESSION03/BAI02/Main.java
@@ -0,0 +1,20 @@
+package SESSION03.Main;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        User alice   = new User("alice",   "alice@gmail.com",   Status.ACTIVE);
+        User bob     = new User("bob",     "bob@yahoo.com",     Status.INACTIVE);
+        User charlie = new User("charlie", "charlie@gmail.com", Status.ACTIVE);
+        User david   = new User("david",   "david@outlook.com", Status.ACTIVE);
+        User eve     = new User("eve",     "eve@gmail.com",     Status.INACTIVE);
+
+        List<User> users = List.of(alice, bob, charlie, david, eve);
+
+        System.out.println("Username của người dùng có email @gmail.com:");
+        users.stream()
+             .filter(user -> user.email().endsWith("@gmail.com"))
+             .forEach(user -> System.out.println(user.username()));
+    }
+}
