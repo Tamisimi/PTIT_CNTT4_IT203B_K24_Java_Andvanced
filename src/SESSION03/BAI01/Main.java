291aafa80adb2b1ba2e28dc155a7a0410d9c5d42
Tamisimi <alexmd0107@gmail.com>
2026-03-10 23:42:17 +0700

    Update Main.java

--- src/SESSION03/BAI01/Main.java
+++ src/SESSION03/BAI01/Main.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        User alice   = new User("alice",   "alice@example.com",   Status.ACTIVE);
-        User bob     = new User("bob",     "bob@example.com",     Status.INACTIVE);
-        User charlie = new User("charlie", "charlie@example.com", Status.ACTIVE);
+        User loc   = new User("loc",   "loc@gmail.com",   Status.ACTIVE);
+        User minh     = new User("minh",     "minh@example.com",     Status.INACTIVE);
+        User thang = new User("thang", "thang@example.com", Status.ACTIVE);
 
-        List<User> users = List.of(alice, bob, charlie);
+        List<User> users = List.of(loc, minh, thang);
 
         users.stream().forEach(System.out::println);
     }
