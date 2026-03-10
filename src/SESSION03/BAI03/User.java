126fb38fa0d6ecd0f09f5c28ff4b4f65d6ae81a1
Tamisimi <alexmd0107@gmail.com>
2026-03-10 23:37:42 +0700

    Create User.java

---
+++ src/SESSION03/BAI03/User.java
@@ -0,0 +1,17 @@
+package SESSION03.SESSION03_03;
+
+public class User {
+    private String username;
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
