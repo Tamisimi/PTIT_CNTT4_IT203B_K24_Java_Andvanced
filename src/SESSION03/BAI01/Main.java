0233108272096ab29f9bf1366bf8032e4e10c143
Tamisimi <alexmd0107@gmail.com>
2026-03-10 23:33:39 +0700

    Update Main.java

--- src/SESSION03/BAI01/Main.java
+++ src/SESSION03/BAI01/Main.java
@@ -1,23 +1,15 @@
-package SESSION03.Main;
+package SESSION03.BAI01;
 
 import java.util.List;
 
 public class Main {
-    
     public static void main(String[] args) {
-        
         User alice   = new User("alice",   "alice@example.com",   Status.ACTIVE);
         User bob     = new User("bob",     "bob@example.com",     Status.INACTIVE);
         User charlie = new User("charlie", "charlie@example.com", Status.ACTIVE);
-        
+
         List<User> users = List.of(alice, bob, charlie);
-        
-        System.out.println("Danh sách người dùng:");        
-        users.forEach(user -> {
-            System.out.printf("Username: %-10s | Email: %-25s | Status: %s%n",
-                user.username(),
-                user.email(),
-                user.status());
-        });
+
+        users.stream().forEach(System.out::println);
     }
 }
