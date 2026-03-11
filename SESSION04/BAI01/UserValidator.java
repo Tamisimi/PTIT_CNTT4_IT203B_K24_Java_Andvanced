adb5ee1e2eadeb1b34d0e3794edcd8f24169fb55
Tamisimi <alexmd0107@gmail.com>
2026-03-11 21:47:22 +0700

    Update UserValidator.java

--- SESSION04/BAI01/UserValidator.java
+++ SESSION04/BAI01/UserValidator.java
@@ -2,7 +2,9 @@ package SESSION04.BAI01;
 
 public class UserValidator {
     public boolean isValidUsername(String username) {
-        if (username == null) return false;
+        if (username == null){
+            return false;
+        }
         int len = username.length();
         return len >= 6 && len <= 20 && !username.contains(" ");
     }
