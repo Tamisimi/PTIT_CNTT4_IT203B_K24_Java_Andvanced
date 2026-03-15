5027b481a9b49aa90306c5961ab346d61e7ccfc3
Tamisimi <alexmd0107@gmail.com>
2026-03-16 00:02:21 +0700

    Create Ticket.java

---
+++ SESSION06/BAI06/Ticket.java
@@ -0,0 +1,23 @@
+package SESSION06.BAI06;
+
+public class Ticket {
+
+    private String id;
+    private boolean sold = false;
+
+    public Ticket(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public boolean isSold() {
+        return sold;
+    }
+
+    public void sell() {
+        sold = true;
+    }
+}
