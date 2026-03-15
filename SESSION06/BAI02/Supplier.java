78b591a21837d575ea86bf2db74f5777d7e2b141
Tamisimi <alexmd0107@gmail.com>
2026-03-15 23:49:44 +0700

    Create Supplier.java

---
+++ SESSION06/BAI02/Supplier.java
@@ -0,0 +1,23 @@
+package SESSION06.BAI02;
+
+public class Supplier extends Thread {
+
+    private TicketPool pool;
+
+    public Supplier(TicketPool pool) {
+        this.pool = pool;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+
+            Thread.sleep(8000);
+
+            pool.addTickets(3);
+
+        } catch (InterruptedException e) {
+        }
+    }
+}
