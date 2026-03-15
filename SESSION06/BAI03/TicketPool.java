3e382b7d1c04a08e314a72acbba5f608482d08b4
Tamisimi <alexmd0107@gmail.com>
2026-03-15 23:54:31 +0700

    Create TicketPool.java

---
+++ SESSION06/BAI03/TicketPool.java
@@ -0,0 +1,50 @@
+package SESSION06.BAI03;
+
+import session06.hw01.Ticket;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TicketPool {
+
+    private Queue<Ticket> tickets = new LinkedList<>();
+    private String roomName;
+    private int ticketCounter = 1;
+
+    public TicketPool(String roomName, int initial) {
+        this.roomName = roomName;
+
+        for (int i = 0; i < initial; i++) {
+            tickets.add(new Ticket(roomName + "-" + String.format("%03d", ticketCounter++)));
+        }
+    }
+
+    public synchronized Ticket sellTicket() {
+
+        if (tickets.isEmpty()) {
+            return null;
+        }
+
+        return tickets.poll();
+    }
+
+    public synchronized void addTickets(int count) {
+
+        for (int i = 0; i < count; i++) {
+
+            tickets.add(new Ticket(
+                    roomName + "-" + String.format("%03d", ticketCounter++)
+            ));
+        }
+
+        System.out.println("Nhà cung cấp: Đã thêm "
+                + count + " vé vào phòng " + roomName);
+    }
+
+    public synchronized int getRemainingTickets() {
+        return tickets.size();
+    }
+}
