75b4c55dd33a3f25fcbbda1f433f0aa9a1e88f9f
Tamisimi <alexmd0107@gmail.com>
2026-03-15 23:47:59 +0700

    Create BookingCounter.java

---
+++ SESSION06/BAI02/BookingCounter.java
@@ -0,0 +1,26 @@
+package SESSION06.BAI02;
+
+public class BookingCounter extends Thread {
+
+    private String name;
+    private TicketPool pool;
+
+    public BookingCounter(String name, TicketPool pool) {
+        this.name = name;
+        this.pool = pool;
+    }
+
+    @Override
+    public void run() {
+
+        while (true) {
+
+            pool.sellTicket(name);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+}
