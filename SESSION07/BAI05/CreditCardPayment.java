877190323c38ad5903be73f79117e4a2d31c998f
Tamisimi <alexmd0107@gmail.com>
2026-03-16 22:34:54 +0700

    Create CreditCardPayment.java

---
+++ SESSION07/BAI05/CreditCardPayment.java
@@ -0,0 +1,8 @@
+package SESSION07.BAI05;
+
+public class CreditCardPayment implements PaymentMethod {
+
+    public void pay(double amount){
+        System.out.println("Thanh toán thẻ tín dụng: " + amount);
+    }
+}
