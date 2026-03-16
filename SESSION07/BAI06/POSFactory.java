68145139a7c72ad15c331d9c212605df92efb061
Tamisimi <alexmd0107@gmail.com>
2026-03-16 22:47:07 +0700

    Create POSFactory.java

---
+++ SESSION07/BAI06/POSFactory.java
@@ -0,0 +1,19 @@
+package SESSION07.BAI06;
+
+public class POSFactory implements SalesChannelFactory {
+
+    @Override
+    public DiscountStrategy createDiscount() {
+        return new MemberDiscount();
+    }
+
+    @Override
+    public PaymentMethod createPayment() {
+        return new CashPayment();
+    }
+
+    @Override
+    public NotificationService createNotification() {
+        return new ReceiptPrinter();
+    }
+}
