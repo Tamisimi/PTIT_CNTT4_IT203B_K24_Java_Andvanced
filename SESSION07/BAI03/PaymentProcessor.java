d01be85bfb93b3f54966fa55b7f56f541e7bbbf0
Tamisimi <alexmd0107@gmail.com>
2026-03-16 22:22:58 +0700

    Create PaymentProcessor.java

---
+++ SESSION07/BAI03/PaymentProcessor.java
@@ -0,0 +1,14 @@
+package SESSION07.BAI03;
+
+public class PaymentProcessor {
+
+    private PaymentMethod paymentMethod;
+
+    public PaymentProcessor(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public void process(double amount) {
+        paymentMethod.pay(amount);
+    }
+}
