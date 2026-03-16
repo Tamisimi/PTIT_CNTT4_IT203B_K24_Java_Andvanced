646c00f22e4e1ab6ab450dab8e2f9b9defcae298
Tamisimi <alexmd0107@gmail.com>
2026-03-16 22:26:04 +0700

    Create EmailService.java

---
+++ SESSION07/BAI04/EmailService.java
@@ -0,0 +1,9 @@
+package SESSION07.BAI04;
+
+public class EmailService implements NotificationService {
+
+    @Override
+    public void send(String message, String recipient) {
+        System.out.println("Gửi email: " + message);
+    }
+}
