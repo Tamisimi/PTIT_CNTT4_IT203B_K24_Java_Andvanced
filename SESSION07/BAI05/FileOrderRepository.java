1632c2b972e6b56bd1f3063d4d13b74677c1fc7f
Tamisimi <alexmd0107@gmail.com>
2026-03-16 22:36:23 +0700

    Create FileOrderRepository.java

---
+++ SESSION07/BAI05/FileOrderRepository.java
@@ -0,0 +1,17 @@
+package SESSION07.BAI05;
+
+import java.util.*;
+
+public class FileOrderRepository implements OrderRepository {
+
+    private List<Order> orders = new ArrayList<>();
+
+    public void save(Order order){
+        orders.add(order);
+        System.out.println("Đã lưu đơn hàng " + order.getId());
+    }
+
+    public List<Order> findAll(){
+        return orders;
+    }
+}
