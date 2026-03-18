04ad1845cd95bb7aa883925966aefcdd0a0c13a4
Tamisimi <alexmd0107@gmail.com>
2026-03-18 19:05:02 +0700

    Create Fan.java

---
+++ SESSION08/BAI03/Fan.java
@@ -0,0 +1,11 @@
+package SESSION08.BAI03;
+
+class Fan {
+    public void on() {
+        System.out.println("Quạt: Bật");
+    }
+
+    public void off() {
+        System.out.println("Quạt: Tắt");
+    }
+}
