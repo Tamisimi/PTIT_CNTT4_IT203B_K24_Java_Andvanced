fd4b2256a3807adb6674558a611268fe42217778
Tamisimi <alexmd0107@gmail.com>
2026-03-18 19:01:51 +0700

    Create Light.java

---
+++ SESSION08/BAI01/Light.java
@@ -0,0 +1,11 @@
+package SESSION08.BAI01;
+
+class Light implements Device {
+    public void turnOn() {
+        System.out.println("Đèn: Bật sáng.");
+    }
+
+    public void turnOff() {
+        System.out.println("Đèn: Tắt.");
+    }
+}
