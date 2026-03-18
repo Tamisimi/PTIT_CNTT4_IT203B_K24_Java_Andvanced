fb94274ff3317cdeb9f81984f324376a4480103b
Tamisimi <alexmd0107@gmail.com>
2026-03-18 18:59:28 +0700

    Create AirConditionerFactory.java

---
+++ SESSION08/BAI01/AirConditionerFactory.java
@@ -0,0 +1,8 @@
+package SESSION08.BAI01;
+
+class AirConditionerFactory extends DeviceFactory {
+    public Device createDevice() {
+        System.out.println("AirConditionerFactory: Đã tạo điều hòa mới.");
+        return new AirConditioner();
+    }
+}
