6bbfdb337ce9a3794c487fb5022edab9292bc173
Tamisimi <alexmd0107@gmail.com>
2026-03-18 18:58:58 +0700

    Create AirConditioner.java

---
+++ SESSION08/BAI01/AirConditioner.java
@@ -0,0 +1,11 @@
+package SESSION08.BAI01;
+
+class AirConditioner implements Device {
+    public void turnOn() {
+        System.out.println("Điều hòa: Bật.");
+    }
+
+    public void turnOff() {
+        System.out.println("Điều hòa: Tắt.");
+    }
+}
