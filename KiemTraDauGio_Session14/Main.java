b9253f8384aa7cf55cdf39de3a57745fedcbceb4
Tamisimi <alexmd0107@gmail.com>
2026-03-25 09:43:41 +0700

    Update Main.java

--- KiemTraDauGio_Session14/Main.java
+++ KiemTraDauGio_Session14/Main.java
@@ -5,10 +5,8 @@ public class Main {
 
         TransferService service = new TransferService();
 
-        // Thực hiện chuyển khoản (ví dụ: ACC01 chuyển 1500 cho ACC02)
         service.transfer("ACC01", "ACC02", 1500.00);
 
-        // Hiển thị kết quả cuối cùng để đối soát
         service.printAccounts();
     }
 }
