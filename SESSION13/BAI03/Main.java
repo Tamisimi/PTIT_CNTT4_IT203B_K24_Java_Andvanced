b5e7347d1b7bf8036970bc3e8ff7cdb967a2a7ed
Tamisimi <alexmd0107@gmail.com>
2026-03-25 07:07:48 +0700

    Create Main.java

---
+++ SESSION13/BAI03/Main.java
@@ -0,0 +1,9 @@
+package SESSION13.BAI03;
+
+public class Main {
+    public static void main(String[] args) {
+        HospitalService service = new HospitalService();
+
+        service.xuatVienVaThanhToan(1, 500);
+    }
+}
